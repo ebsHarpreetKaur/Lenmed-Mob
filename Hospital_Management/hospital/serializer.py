b426acda49b3ca5e5b48a7c1e2357b81752997fe
GurkiratSingh00059 <gcrew.gurkirats@gmail.com>
2023-07-11 11:51:13 +0530

    delete-apis
    delete-apis

--- Hospital_Management/hospital/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Hospital
-
-
-class HospitalSerializer(serializers.ModelSerializer):
-    """ Hospital fields to return in response"""
-    class Meta:
-        model = Hospital
-        fields = ('id', 'admin_email', 'name', 'admin', 'address')
